74e39297bc8761d94a5ed2022e30e13bddca4846
Tamir Duberstein <tamird@gmail.com>
2017-11-27 15:24:12 -0500

    Update Rust dependencies

--- rusty/build.rs
+++ rusty/build.rs
@@ -1,5 +1,6 @@
 extern crate cheddar;
 
 fn main() {
-	cheddar::Cheddar::new().unwrap().run_build("include/rusty.h");
+	cheddar::Builder::c99().expect("could not read cargo manifest")
+            .name("rusty.h").output_directory("include").run_build();
 }
